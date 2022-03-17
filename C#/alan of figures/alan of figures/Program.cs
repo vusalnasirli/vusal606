6fb18d8b7129931345833b0e2b81fd8f406ffcc8
vusalnasirli <7bgzt9j@code.edu.az>
2022-03-17 08:45:03 +0400

    hometask

---
+++ C#/alan of figures/alan of figures/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace alan_of_figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateArea(2));
+            Console.WriteLine(CalculateArea(2, 3));
+            Console.WriteLine(CalculateArea(2, 3, 4));
+        }
+
+
+        static int CalculateArea(int a)
+        {
+            int area = 3 * a * a;
+            return area;
+        }
+        static int CalculateArea(int a, int b)
+        {
+            int area = a * b;
+            return area;
+        }
+        static int CalculateArea(int a, int b, int c)
+        {
+            int area = 2 * (a * b + a * c + b * c);
+            return area;
+        }
+
+    }
+}
+
